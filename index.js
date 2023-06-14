f50dc5e56bbe17c9e2723fe2025ad999a004dcef
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-06-13 22:35:33 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    return customerName;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return customerName;
+}
+
+const leastFavoriteCustomer = "sally"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "karen";
+    return customerName;
+}
+
